664dca3a2b2dd3c0f1da5c84b610fc1c8b873cff
seanpoke <33081070+seanpoke@users.noreply.github.com>
2026-06-11 23:22:55 -0700

    Initial Commit

---
+++ PasswordManager/PasswordManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PasswordManagerApp.swift
+//  PasswordManager
+//
+//  Created by Workstation on 6/11/26.
+//
+
+import SwiftUI
+
+@main
+struct PasswordManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
